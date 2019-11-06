3bd63cfab11e60b421800acafc0e420945dc9ffa
Dmitry Novosyolov <i1710@icloud.com>
2019-11-06 05:42:02 +0200

    Initial Commit

---
+++ Drawing { Paul Hudson }/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing { Paul Hudson }
+//
+//  Created by Dmitry Novosyolov on 06/11/2019.
+//  Copyright © 2019 Dmitry Novosyolov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
